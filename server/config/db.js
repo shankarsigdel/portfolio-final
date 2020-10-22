76b5c8a2616d0768be9c4457f45c8ac860340beb
LAPTOP-NKGJ4DT9\Admin <sss.sgd26@gmail.com>
2020-10-22 13:54:59 -0400

    Initial Commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports = 
 {
     //"URI": "mongodb://localhost/business_contact",
-    "URI": "mongodb+srv://Shankar:NUUozR3KpMXnepRs@project.qw706.mongodb.net/contact_content?retryWrites=true&w=majority",
+    "URI": "mongodb+srv://Shankar:NUUozR3KpMXnepRs@project.qw706.mongodb.net/business_contacts?retryWrites=true&w=majority",
     "Secret": 'SomeSecret'
 }
